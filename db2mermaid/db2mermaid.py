ea6150a064948e50edf1a9fc3e62b5ddeba8ee04
kitsystemyou <kitsystemyou@gmail.com>
2023-03-19 08:31:24 +0900

    Refactor func name, comment

--- db2mermaid/db2mermaid.py
+++ db2mermaid/db2mermaid.py
@@ -2,45 +2,48 @@ from sqlalchemy import create_engine, MetaData
 
 
 class DB2Mermaid:
-    _user = ""
-    _password = ""
-    _host = ""
-    _port = ""
-    _db_name = ""
-    _db_uri = ""
 
     def __init__(self):
-        self._user = "user"
-        self._password = "password"
-        self._host = "host"
-        self._port = "3306"
-        self._db_name = "db_name"
-        self._db_uri = f"mysql://{self._user}:{self._password}@{self._host}:{self._port}/{self._db_name}"
+        self.user = "user"
+        self.password = "password"
+        self.host = "host"
+        self.port = "3306"
+        self.db_name = "db_name"
 
-        engine = create_engine(self._db_uri)
-        self.meta = MetaData()
-        self.meta.reflect(bind=engine)
-
-    def set_db_config(self, user, password, host, port, db_name):
+    def init_db(self, user, password, host, port, db_name):
         self.user = user
         self.password = password
         self.host = host
         self.port = port
         self.db_name = db_name
+        self.db_url = f"mysql://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}"
+        print("db_url: ", self.db_url)
+        engine = create_engine(self.db_url)
+        self.meta = MetaData()
+        self.meta.reflect(bind=engine)
 
-    def write_texts(self, lines: list[str]):
+    def _write_texts(self, lines: list[str]):
         f = open('er.md', 'a', encoding='UTF-8')
         f.writelines(lines)
         f.close()
 
-    def init_text(self):
-        self.write_texts(["```mermaid\n", "erDiagram\n"])
+    def _init_text(self):
+        self._write_texts(["```mermaid\n", "erDiagram\n"])
 
-    def close_text(self):
-        self.write_texts(["```"])
+    def _close_text(self):
+        self._write_texts(["```"])
 
     def create_row_per_column(self, column_data) -> str:
-        # gettable: name, type, autoincrement, default, nullable, primary_key, foreign_key, index, comment
+        """_summary_
+
+        Args:
+            column_data (sqlalchemy.schema.Column): Column data which contains name, type, autoincrement,
+            default, nullable, primary_key, foreign_key, index, comment
+
+        Returns:
+            str: string of column data for mermaid erDiagram row
+        """
+
         line = f"        {column_data.type} {column_data.name}"
         if column_data.primary_key:
             line += " PK"
@@ -52,7 +55,7 @@ class DB2Mermaid:
             return line + "\n"
 
     def generate(self):
-        self.init_text()
+        self._init_text()
 
         for n, t in self.meta.tables.items():
             texts: list[str] = []
@@ -61,13 +64,12 @@ class DB2Mermaid:
             for c in t.columns.values():
                 texts.append(self.create_row_per_column(c))
             texts.append("}\n")
-            self.write_texts(texts)
+            self._write_texts(texts)
 
-        self.close_text()
+        self._close_text()
 
 
 if __name__ == "__main__":
     d = DB2Mermaid()
-    d.set_db_config("user", "pass", "localhost", "3306", "db_name")
-    print(d._db_uri)
+    d.init_db("root", "pass", "127.0.0.1", "3307", "charamane")
     d.generate()
